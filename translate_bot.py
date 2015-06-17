3d2b77d6ad0508b48d44fceadd1e4abcba2e3240
cappachu <cappachu@gmail.com>
2015-06-17 11:10:26 -0400

    add symmetric word distance

--- translate_bot.py
+++ translate_bot.py
@@ -4,12 +4,14 @@ import requests
 import random
 import operator
 import editdistance 
+from sets import Set
 from twython import Twython
 
 # TODO exception 
 # TODO length restriction on twitter posts?
 # TODO edit dist?
 # TODO random choice for subset of all languages or use all langs
+# TODO use of color in languages, connotations
 
 
 def get_quote():
@@ -31,23 +33,31 @@ def translate(quote):
     # the values are pairs containing the quote
     # translated into the destination language and
     # the reverse-translation in the source language
+    # TODO remove source lang 'en'
+    # TODO use source and target lang?
     gs = goslate.Goslate()
     all_langs = gs.get_languages().keys()
-    # TODO remove source lang 'en'
-    num_langs = 10
-    assert(len(all_langs) > num_langs)
+
+    num_langs = 10 #len(all_langs)
+    assert(len(all_langs) >= num_langs)
     lang_indices = random.sample(xrange(len(all_langs)), num_langs)
-    # TODO use source and target lang?
     quote_dict = {all_langs[lang_index]: gs.translate(gs.translate(quote, all_langs[lang_index]), 'en') for lang_index in lang_indices}
+    
     return quote_dict
+
+def symmetric_word_dist(quote_one, quote_two):
+    words_one = Set(quote_one.split())
+    words_two = Set(quote_two.split())
+    symmetric_diff = words_one ^ words_two
+    return len(symmetric_diff)
 
 def find_furthest_quote(quote, quote_dict):
     """finds a reverse-translated quote that is most different
     from the original quote"""
-    #lang = quote_dict.keys()[0] 
-    #furthest_quote = quote_dict[lang]
-    furthest_quote = max(quote_dict.iteritems(), key=lambda q: editdistance.eval(q, quote))
-    #print 'MAX:', furthest_quote
+    # edit distance
+    #furthest_quote = max(quote_dict.iteritems(), key=lambda qtuple: editdistance.eval(qtuple[1], quote))
+    # symmetric difference of words
+    furthest_quote = max(quote_dict.iteritems(), key=lambda qtuple: symmetric_word_dist(qtuple[1], quote))
     return furthest_quote
 
 def tweet(quote, reverse_quote, lang, author):
@@ -60,9 +70,7 @@ def tweet(quote, reverse_quote, lang, author):
 def main():
     quote, author = get_quote()
     quote_dict = translate(quote)
-    print 'quote_dict:', quote_dict
     lang, furthest_quote = find_furthest_quote(quote, quote_dict)
-    print 'furthest quote:', furthest_quote
     tweet(quote, furthest_quote, lang, author)
     
             
